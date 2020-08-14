d06ae82b3a79672f553acb1c5ceb2ea0339ebe30
djsteve33 <swomble@aol.com>
2020-08-14 17:27:43 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,8 +14,13 @@ class SongsController < Sinatra::Base
   end
 
   get '/songs/new' do
-    @genres = Genre.all
+    #@genres = Genre.all
     erb :"songs/new"
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :"songs/show"
   end
 
   post '/songs' do
@@ -36,46 +41,67 @@ class SongsController < Sinatra::Base
 
     @song.save
 
+    
+      # @song = Song.create(:name => params["Name"])
+      # @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+      # @song.genre_ids = params[:genres]
+      # @song.save
+
     flash[:message] = "Successfully created song."
-    redirect to "songs/#{@song.slug}"
+    redirect to "songs/show"
 
   end
 
-  get '/songs/:slug' do
-    slug = params[:slug]
-    @song = Song.find_by_slug(slug)
-    erb :"songs/show"
-  end
+ 
 
-  patch '/songs/:slug' do
-    song = Song.find_by_slug(params[:slug])
-    song.name = params[:song][:name]
+  # patch '/songs/:slug' do
+  #   song = Song.find_by_slug(params[:slug])
+  #   song.name = params[:song][:name]
 
-    artist_name = params[:song][:artist]
-    if Artist.find_by(:name => artist_name)
-      if song.artist.name != artist_name
-        song.artist = Artist.find_by(:name => artist_name)
-      end
-    else
-      song.artist = Artist.create(:name => artist_name)
-    end
+  #   artist_name = params[:song][:artist]
+  #   if Artist.find_by(:name => artist_name)
+  #     if song.artist.name != artist_name
+  #       song.artist = Artist.find_by(:name => artist_name)
+  #     end
+  #   else
+  #     song.artist = Artist.create(:name => artist_name)
+  #   end
 
-    if song.genres
-      song.genres.clear
-    end
-    genres = params[:song][:genres]
-    genres.each do |genre|
-      song.genres << Genre.find(genre)
-    end
+  #   if song.genres
+  #     song.genres.clear
+  #   end
+  #   genres = params[:song][:genres]
+  #   genres.each do |genre|
+  #     song.genres << Genre.find(genre)
+  #   end
 
-    song.save
-    flash[:message] = "Successfully updated song."
-    redirect to "songs/#{song.slug}"
-  end
+  #   song.save
+  #   flash[:message] = "Successfully updated song."
+  #   redirect to "songs/#{song.slug}"
+  # end
+
+  # get '/songs/:slug/edit' do
+  #   slug = params[:slug]
+  #   @song = Song.find_by_slug(slug)
+  #   erb :"songs/edit"
+  # end
 
   get '/songs/:slug/edit' do
-    slug = params[:slug]
-    @song = Song.find_by_slug(slug)
-    erb :"songs/edit"
+    @song = Song.find_by_slug(params[:slug])
+  
+    erb :'songs/edit'
   end
+  
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+  
+    @song.update(params[:song])
+  
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect to "songs/show"
+   end
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ActiveRecord::Base
     #Artist::Slugifiable
     has_many :songs
-    has_many :genres, through: :songs
+    has_many :genres, :through => :songs
     extend Slugifiable::ClassMethods
     include Slugifiable::InstanceMethods
   end
